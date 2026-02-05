46215bee6bf970535ade2b009be8aad691f58d0d
cjspersona <hickson_cory@students.columbusstate.edu>
2026-02-04 19:44:42 -0500

    Add favorite hobby field into student profile

--- student_info.py
+++ student_info.py
@@ -8,6 +8,7 @@ major = "Information Technology"
 graduation_year = 2025
 email = "hickson_cory@columbusstate.edu"
 hometown = "Roberta, Ga"
+favoriteHobby = "Playing video games or working out"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
@@ -17,6 +18,7 @@ print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
 print(f"Email📧: {email}")
 print(f"Hometown🏠: {hometown}")
+print(f"FavoriteHobby: {favoriteHobby}")
 print("==========================================")
 
 
